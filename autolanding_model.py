36537989729cbdfb5bb252c42719044b949041f6
Shaon Majumder <shaon.fdr@robist.com>
2018-07-16 20:29:04 +0600

    continuing

--- autolanding_model.py
+++ autolanding_model.py
@@ -8,6 +8,13 @@ from sklearn import neighbors,svm
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn.naive_bayes import GaussianNB,BernoulliNB,MultinomialNB
 from sklearn import tree
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
 
 data = pd.read_csv('data/shuttle-landing-control.csv',names=['auto_control','stability','error','sign','wind','magnitude','visibility'])
 ## |---------- Data Set Properties ------------|
@@ -26,79 +33,35 @@ features = data.ix[:,1:]
 ## Scalling has no effect on this data,
 #Assuming that it is most of few categorical data
 #features = StandardScaler().fit_transform(features)
-target = data[data.columns[0]]
+target = data.ix[:,0]
+
 
 accuracies = []
 models = []
 
-clf =  LinearRegression()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
+KMeans
+classifiers = [QuadraticDiscriminantAnalysis(),MLPClassifier(alpha=1),AdaBoostClassifier(),RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),GaussianProcessClassifier(1.0 * RBF(1.0)),tree.DecisionTreeClassifier(),LinearRegression(),NearestCentroid(),neighbors.KNeighborsClassifier(),svm.LinearSVC()] + [svm.SVC(kernel = kernel_function) for kernel_function in ['rbf','linear', 'poly', 'rbf', 'sigmoid','linear'] ] + [svm.SVC(gamma=2, C=1, kernel = kernel_function) for kernel_function in ['rbf','linear', 'poly', 'rbf', 'sigmoid','linear'] ] + [svm.SVR(),GaussianNB(),BernoulliNB(),MultinomialNB()]
+model_names = ['1']*len(classifiers)
+model_names[23-1] = 'Regression SVR'
+model_names[23:27] = ['naive_bayes']*3
+print(model_names)
+print(model_names.index("Regression SVR"))
 
-clf = NearestCentroid()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
+###SVM > 'precomputed' kernel function does not work, find the reason behind it, may be there is somme shape problem of datas
 
-
-clf = neighbors.KNeighborsClassifier()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
-
-
-#SVM
-## 'precomputed' kernel function does not work, find the reason behind it, may be there is somme shape problem of datas
-for kernel_function in ['rbf','linear', 'poly', 'rbf', 'sigmoid']:
-    clf = svm.SVC(kernel = kernel_function)
+## Running all the model
+for class_model in classifiers:
+    clf = class_model
     clf.fit(features,target)
     accuracies.append(clf.score(features,target))
     models.append(str(clf))
 
 
 
-# Linear SVC
-clf = svm.LinearSVC()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
-
-# Regression SVR
-clf = svm.SVR()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
-
-
-# naive_bayes
-clf =  GaussianNB()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
-
-# naive_bayes
-clf =  BernoulliNB()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
-
-# naive_bayes
-clf =  MultinomialNB()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
-
-# Decision Tree
-clf = tree.DecisionTreeClassifier()
-clf.fit(features,target)
-accuracies.append(clf.score(features,target))
-models.append(str(clf))
-
-
 for c,m in sorted(zip(accuracies,models),reverse=True):
     print(m)
     print("Accuracy: %s \n"%c)
+
 
 
 
